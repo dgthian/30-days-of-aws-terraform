5e7d296ddff56134ca3dd6d795b00e25e43f2864
Djibril Gueye Thiandoum <djibrilgueye.thiandoum@sagam.sn>
2026-05-30 21:57:29 +0000

    day04: Update infra

--- day04/main.tf
+++ day04/main.tf
@@ -21,12 +21,62 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "environment" {
+  description = "The environment for the resources"
+  type        = string
+  default     = "ev"
+}
+
+variable channel_name {
+  description = "The name of the channel"
+  type        = string
+  default     = "ttwp"
+}
+
+variable region {
+  description = "The AWS region to create resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+locals {
+  bucket_name = "lab-bucket-${var.environment}"
+  vpc_name = "${var.environment}-VPC"
+}
+
 # Create s3 bucket
 resource "aws_s3_bucket" "first_bucket" {
-  bucket = "lab-bucket-terraform-2026"
+  bucket = local.bucket_name
 
   tags = {
-    Name        = "My bucket 2.0"
-    Environment = "Dev"
+    Name        = "local.bucket_name"
+    Environment = var.environment
   }
+}
+
+resource "aws_vpc" "sample" {
+  cidr_block = "10.0.0.0/16"
+  region     = var.region
+  tags = {
+    Environment = var.environment
+    Name        = local.vpc_name
+    }
+}
+
+resource "aws_instance" "sample" {
+  ami           = "ami-00e801948462f718a"
+  instance_type = "t3.micro"
+  region        = var.region
+  tags = {
+    Environment = var.environment
+    Name        = "${var.environment}-EC2-Instance"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.sample.id
+}
+
+output "instance_id" {
+  value = aws_instance.sample.id
 }
