ddf84c0abd190f37fca047e6c3ba44079151013b
Djibril Gueye Thiandoum <djibrilgueye.thiandoum@sagam.sn>
2026-05-31 21:38:09 +0000

    day07: AWS Terraform Type Constraints

---
+++ day07/main.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "sample" {
+  count = var.instance_count
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type = var.allowed_vm_types[0]
+  #region        = var.allow_region[0] 
+  region        = var.config.region
+  monitoring     = var.monitoring
+  associate_public_ip_address = var.associate_public_ip
+
+  tags =  var.tags
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = var.tags_security_group
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = var.cidr_block[0]
+  from_port         = var.ingress_values[0]
+  ip_protocol       = var.ingress_values[1]
+  to_port           = var.ingress_values[2]
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
---
+++ day07/variables.tf
@@ -0,0 +1,93 @@
+variable "environment" {
+  description = "The environment for the resources"
+  type        = string
+  default     = "staging"
+}
+
+variable channel_name {
+  description = "The name of the channel"
+  type        = string
+  default     = "ttwp"
+}
+
+variable region {
+  description = "The AWS region to create resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable instance_count {
+  description = "Number of EC2 instances to create"
+  type        = number
+}
+
+variable "monitoring" {
+  description = "Enable detailed monitoring for EC2 instances"
+  type        = bool
+  default     = true
+}
+
+variable "associate_public_ip" {
+  description = "Whether to associate a public IP address with the EC2 instances"
+  type        = bool
+  default     = true
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = list(string)
+  default     = ["10.0.0.0/8", "192.168.0.0/16", "172.16.0.0/12"]
+}
+
+variable "allowed_vm_types" {
+  description = "List of allowed VM types for security group ingress rules"
+  type        = list(string)
+  default     = ["t3.micro", "t3.small", "t3.medium"]
+}
+
+variable allow_region {
+  description = "The AWS region to create resources in"
+  type        = list(string)
+  default     = ["us-east-1", "us-west-2", "eu-west-1"]
+}
+
+variable "tags" {
+  description = "A map of tags to apply to resources"
+  type        = map(string)
+  default     = {
+    Environment = "dev"
+    Name        = "dev-EC2-Instance"
+    created_by  = "Terraform"
+  }
+}
+
+variable "tags_security_group" {
+  description = "A map of tags to apply to security group resources"
+  type        = map(string)
+  default     = {
+    Name = "allow_tls"
+  }
+}
+
+variable "ingress_values" {
+  description = "A list of maps defining ingress rules for the security group"
+  type        =  tuple([number, string, number])
+  default     = [443, "tcp", 443] 
+}
+
+variable "config" {
+  type = object({
+    environment = string
+    region      = string
+    monitoring   = bool
+    associate_public_ip = bool
+    instance_count = number
+  })
+  default = {
+    environment = "staging"
+    region      = "us-east-1"
+    monitoring   = true
+    associate_public_ip = true
+    instance_count = 1
+  }
+}
